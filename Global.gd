a4d481fc2d8f1865931aba13d571922f6032824c
Erika Becker <97013344+eribecke@users.noreply.github.com>
2023-03-26 05:35:58 -0400

    Created player

--- Global.gd
+++ Global.gd
@@ -1,5 +1,9 @@
 extends Node
 
-func _unhandled_input(event):
-	if event.is_action_pressed("quit"):
-		get_tree().quit()
+const SAVE_PATH = "user://savegame.sav"
+const SECRET = "C220 Is the Best!"
+var save_file = ConfigFile.new()
+onready var HUD = get_node_or_null("/root/Game/UI/HUD")
+onready var Game = load("res://Game.tscn")
+
+
